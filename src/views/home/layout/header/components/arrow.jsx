ebba3295236c3b2e3d97ff6870e6b0e3a516cc03
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-17 19:07:56 +0300

    add down arrow

---
+++ src/views/home/layout/header/components/arrow.jsx
@@ -0,0 +1,16 @@
+export default function Arrow({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="42.766"
+      height="26.394"
+      viewBox="0 0 42.766 26.394">
+      <path
+        id="keyboard_arrow_up"
+        className={className}
+        d="M11.011,34.41,6,29.4,27.383,8.016,48.766,29.4,43.755,34.41,27.383,18.037Z"
+        transform="translate(48.766 34.41) rotate(180)"
+      />
+    </svg>
+  );
+}
--- src/views/home/layout/header/header.jsx
+++ src/views/home/layout/header/header.jsx
@@ -1,10 +1,13 @@
 import image from "../../../../images/background.jpg";
 import styles from "./header.module.scss";
+import Arrow from "./components/arrow";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.imageContainer}>
+    <header
+      className={styles.header}
+      id="header">
+      <div className={styles.container}>
         <img
           className={styles.image}
           src={image}
@@ -22,6 +25,11 @@ export default function Header() {
               The joke juggler who turns your frown upside down!
             </span>
           </h1>
+          <div
+            className={styles.arrowContainer}
+            role="button">
+            <Arrow className={styles.arrow} />
+          </div>
         </div>
       </div>
     </header>
