631aca50a56edd015241ef32a6f132f8bbf430ef
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-17 20:34:14 +0300

    add content to cta

--- src/views/home/layout/cta/cta.jsx
+++ src/views/home/layout/cta/cta.jsx
@@ -6,6 +6,16 @@ export default function CTA() {
     <section
       className={styles.cta}
       id="cta">
+      <div className={styles.content}>
+        <h2 className={styles.h2}>
+          Join the Fun: Yusef Bataineh's Exclusive "Comedy 101" Special!
+        </h2>
+        <p className={styles.p}>
+          Get ready to laugh with Yusef Bataineh's latest stand-up comedy
+          special, "Comedy 101"! Subscribe now and receive your exclusive link
+          via WhatsApp and email. Rest assured, we are spam-free!
+        </p>
+      </div>
       <div className={styles.imageContainer}>
         <img
           className={styles.image}
