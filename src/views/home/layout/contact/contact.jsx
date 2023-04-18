19f42b4ca3c55f804b94dea57c98b0b5bd8b32fa
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-18 11:52:04 +0300

    add animations to contact

--- src/views/home/layout/contact/contact.jsx
+++ src/views/home/layout/contact/contact.jsx
@@ -3,22 +3,29 @@ import Facebook from "./components/facebook";
 import Instagram from "./components/instagram";
 import Twitter from "./components/twitter";
 import Youtube from "./components/youtube";
+import Fade from "react-reveal/Fade";
 
 export default function Contact() {
   return (
     <section className={styles.contact}>
-      <h2 className={styles.h2}>Get in touch!</h2>
-      <div className={styles.iconContainer}>
-        <Facebook className={styles.icon} />
-        <Instagram className={styles.icon} />
-        <Twitter className={styles.icon} />
-        <Youtube className={styles.icon} />
-      </div>
-      <a
-        href="mailto:hello@yusefbataineh.com"
-        className={styles.email}>
-        hello@yusefbataineh.com
-      </a>
+      <Fade
+        top
+        distance="40px"
+        delay={200}
+        duration={1000}>
+        <h2 className={styles.h2}>Get in touch!</h2>
+        <div className={styles.iconContainer}>
+          <Facebook className={styles.icon} />
+          <Instagram className={styles.icon} />
+          <Twitter className={styles.icon} />
+          <Youtube className={styles.icon} />
+        </div>
+        <a
+          href="mailto:hello@yusefbataineh.com"
+          className={styles.email}>
+          hello@yusefbataineh.com
+        </a>
+      </Fade>
     </section>
   );
 }
